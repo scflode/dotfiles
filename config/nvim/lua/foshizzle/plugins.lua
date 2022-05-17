913d3accd6e9128b9ece8035b064a05b8a330c4a
Flo Schuessel <mail@florian-schuessel.de>
2022-05-17 12:01:06 +0200

    Open Packer in floating window

--- config/nvim/lua/foshizzle/plugins.lua
+++ config/nvim/lua/foshizzle/plugins.lua
@@ -5,7 +5,30 @@ if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
 end
 
-return require("packer").startup(function(use)
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init({
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
+})
+
+return packer.startup(function(use)
   use("wbthomason/packer.nvim")
 
   -- UI
