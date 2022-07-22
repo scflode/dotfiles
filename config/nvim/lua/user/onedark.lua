db13f5258d5b06950f78cd995573fc8d294267d7
Flo Schuessel <mail@florian-schuessel.de>
2022-07-22 13:06:09 +0200

    Adapt styling

--- config/nvim/lua/user/onedark.lua
+++ config/nvim/lua/user/onedark.lua
@@ -7,6 +7,14 @@ end
 onedark.setup({
   dark_theme = "onedark",
   light_theme = "onelight",
+  styles = {
+    comments = "italic",
+    functions = "NONE",
+    keywords = "bold,italic",
+    strings = "NONE",
+    variables = "NONE",
+    virtual_text = "NONE",
+  },
 })
 
 vim.cmd("colorscheme onedarkpro")
