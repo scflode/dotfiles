da4f010721f5f79e2c1344db7073ae6439273ca4
Flo Schuessel <mail@florian-schuessel.de>
2022-07-25 09:40:29 +0200

    Change find in tree mapping

--- config/nvim/lua/user/mappings.lua
+++ config/nvim/lua/user/mappings.lua
@@ -12,7 +12,7 @@ map("n", "<Leader>b", ":Telescope buffers<CR>", { noremap = true, silent = true 
 map("n", "<Leader>s", ":Telescope live_grep<CR>", { noremap = true, silent = true })
 map("n", "<Leader>g", ":Telescope git_status<CR>", { noremap = true, silent = true })
 
-map("n", "<Leader>l", ":NvimTreeFindFileToggle<CR>", { noremap = true, silent = true })
+map("n", "<Leader>l", ":NvimTreeFindFile<CR>", { noremap = true, silent = true })
 map("n", "<Leader>n", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 map("n", "<Leader>ct", ":ColorizerToggle<CR>", { noremap = true, silent = true })
