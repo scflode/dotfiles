fa5211c9ea4dc13d9d553ff93f5960386070b674
Flo Schuessel <mail@florian-schuessel.de>
2022-08-10 15:11:46 +0200

    Add battery indicator for tmux

---
+++ scripts/battery.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+pmset -g batt | \
+  tail -n1 | \
+  sed -e 's/.*(id=[[:digit:]]*)[[:space:]]*\(.*\) present: true/\1/' \
+    -e 's/;//g' \
+    -e 's/ discharging/ꜜ/' \
+    -e 's/ remaining//' \
+    -e 's/ AC attached//' \
+    -e 's/ charging/ꜛ/' \
+    -e 's/ finishing charge/ꜛ/' \
+    -e 's/(no estimate)//'
+
