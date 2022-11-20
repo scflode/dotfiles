959b8b41800b9f9d72ed52dac1c9bcbbf408f6b5
Flo Schuessel <mail@florian-schuessel.de>
2022-11-20 18:51:44 +0100

    Change sorting and defaults for Telescope

--- config/nvim/lua/user/telescope.lua
+++ config/nvim/lua/user/telescope.lua
@@ -1,7 +1,21 @@
-local actions = require("telescope.actions")
+local status_ok, telescope = pcall(require, "telescope")
 
-require("telescope").setup({
+if not status_ok then
+  return
+end
+local actions = require("telescope.actions")
+local telescopeConfig = require("telescope.config")
+
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- I don't want to search in the `.git` directory.
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!.git/*")
+
+telescope.setup({
   defaults = {
+    vimgrep_arguments = vimgrep_arguments,
     file_ignore_patterns = { ".git/" },
     mappings = {
       i = {
@@ -10,9 +24,17 @@ require("telescope").setup({
       },
     },
   },
+  pickers = {
+    find_files = {
+      -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+      find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
+    },
+  },
 })
+
+require("telescope.builtin").buffers({ sort_mru = true })
 
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
-require("telescope").load_extension("fzf")
-require("telescope").load_extension("live_grep_args")
+telescope.load_extension("fzf")
+telescope.load_extension("live_grep_args")
