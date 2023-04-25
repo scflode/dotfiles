1538d38b92f45ebceb1e939510353a5e45415b3b
Flo Schuessel <mail@florian-schuessel.de>
2023-04-25 10:53:08 +0200

    Configure elixir-tools

--- config/nvim/lua/user/elixir-tools.lua
+++ config/nvim/lua/user/elixir-tools.lua
@@ -1,1 +1,13 @@
-require("elixir").setup()
+local elixir = require("elixir")
+local elixirls = require("elixir.elixirls")
+elixir.setup({
+  elixirls = {
+    tag = "v0.14.3",
+    settings = elixirls.settings({
+      dialyzerEnabled = true,
+      fetchDeps = false,
+      enableTestLenses = true,
+      suggestSpecs = true,
+    }),
+  },
+})
