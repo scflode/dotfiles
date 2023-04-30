9361718e68afacdd3a6d79c8deaa04486236e0a5
Flo Schuessel <mail@florian-schuessel.de>
2023-04-30 16:17:43 +0200

    Add PHP language server support

--- config/FoShizzle/lua/plugins/languages/php.lua
+++ config/FoShizzle/lua/plugins/languages/php.lua
@@ -8,4 +8,27 @@ return {
       })
     end,
   },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      require("util").add_to_table(opts.ensure_installed, {
+        "intelephense",
+        "twigcs",
+        "php-cs-fixer",
+        "psalm",
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        intelephense = {
+          init_options = {
+            licenceKey = vim.env.INTELEPHENSE_LICENCE_KEY,
+          },
+        },
+      },
+    },
+  },
 }
