fce3b9bc5c308da8cef6dc41a3139c02f5b0f72e
Flo Schuessel <mail@florian-schuessel.de>
2023-04-30 12:13:56 +0200

    Add auto dark mode

--- config/FoShizzle/lua/plugins/colorscheme.lua
+++ config/FoShizzle/lua/plugins/colorscheme.lua
@@ -6,4 +6,20 @@ return {
       "rktjmp/lush.nvim",
     },
   },
+  {
+    "f-person/auto-dark-mode.nvim",
+    init = function()
+      local adm = require("auto-dark-mode")
+      adm.setup({
+        update_interval = 1000,
+        set_dark_mode = function()
+          vim.api.nvim_set_option("background", "dark")
+        end,
+        set_light_mode = function()
+          vim.api.nvim_set_option("background", "light")
+        end,
+      })
+      adm.init()
+    end,
+  },
 }
