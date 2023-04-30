00e441fc56706219ec7608a3a3f9c249c59004e2
Flo Schuessel <mail@florian-schuessel.de>
2023-04-30 17:16:57 +0200

    Setup Elixir language server support

--- config/FoShizzle/lua/plugins/languages/elixir.lua
+++ config/FoShizzle/lua/plugins/languages/elixir.lua
@@ -10,4 +10,26 @@ return {
       })
     end,
   },
+  {
+    "elixir-tools/elixir-tools.nvim",
+    ft = { "elixir", "eex", "heex", "surface" },
+    config = function()
+      local elixir = require("elixir")
+      local elixirls = require("elixir.elixirls")
+
+      elixir.setup({
+        credo = {},
+        elixirls = {
+          enabled = true,
+          settings = elixirls.settings({
+            dialyzerEnabled = true,
+            enableTestLenses = true,
+          }),
+        },
+      })
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
 }
