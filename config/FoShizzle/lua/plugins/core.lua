90c4e37fb827fab0c7e46d38e259cc0755cbf727
Flo Schuessel <mail@florian-schuessel.de>
2023-04-30 14:51:51 +0200

    Add editor tooling

--- config/FoShizzle/lua/plugins/core.lua
+++ config/FoShizzle/lua/plugins/core.lua
@@ -2,4 +2,10 @@ require("config").init()
 
 return {
   { "folke/lazy.nvim", version = "*" },
+
+  -- library used by other plugins
+  { "nvim-lua/plenary.nvim", lazy = true },
+
+  -- makes some plugins dot-repeatable like leap
+  { "tpope/vim-repeat", event = "VeryLazy" },
 }
