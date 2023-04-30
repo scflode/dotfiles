3fe309e72d9ab7db5a9549e03f81c0bebcb35443
Flo Schuessel <mail@florian-schuessel.de>
2023-04-30 22:31:36 +0200

    Add window picker

--- config/FoShizzle/lua/plugins/editor.lua
+++ config/FoShizzle/lua/plugins/editor.lua
@@ -71,6 +71,32 @@ return {
         end,
       })
     end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      {
+        "s1n7ax/nvim-window-picker",
+        tag = "v1.*",
+        init = function()
+          require("window-picker").setup({
+            autoselect_one = true,
+            include_current = false,
+            filter_rules = {
+              -- filter using buffer options
+              bo = {
+                -- if the file type is one of following, the window will be ignored
+                filetype = { "neo-tree", "neo-tree-popup", "notify" },
+
+                -- if the buffer type is one of following, the window will be ignored
+                buftype = { "terminal", "quickfix" },
+              },
+            },
+            other_win_hl_color = "#e35e4f",
+          })
+        end,
+      },
+    },
   },
 
   -- search/replace in multiple files
