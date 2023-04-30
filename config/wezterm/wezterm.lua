6d982963520574eb92738b8b681ef74a48544350
Flo Schuessel <mail@florian-schuessel.de>
2023-04-30 22:46:06 +0200

    Remove window decorations

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -13,6 +13,7 @@ local function scheme_for_appearance(appearance)
 end
 
 return {
+  window_decorations = "NONE",
   color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
   font = wezterm.font({
     family = "VictorMono Nerd Font",
@@ -20,7 +21,7 @@ return {
     harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
   }),
   font_size = 15.0,
-  line_height = 1.2,
+  line_height = 1.4,
   enable_tab_bar = false,
   window_padding = {
     left = 5,
