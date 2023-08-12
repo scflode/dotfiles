d90613ab09a4e1cb6c86e56e226ad31d0adb7912
Flo Schuessel <mail@florian-schuessel.de>
2023-08-12 13:36:20 +0200

    Replace intelephense with phpactor

--- config/FoShizzle/lua/plugins/languages/php.lua
+++ config/FoShizzle/lua/plugins/languages/php.lua
@@ -12,23 +12,15 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       require("util").add_to_table(opts.ensure_installed, {
-        "intelephense",
+        "phpactor",
         "twigcs",
-        "php-cs-fixer",
-        "psalm",
       })
     end,
   },
   {
     "neovim/nvim-lspconfig",
     opts = {
-      servers = {
-        intelephense = {
-          init_options = {
-            licenceKey = vim.env.INTELEPHENSE_LICENCE_KEY,
-          },
-        },
-      },
+      servers = {},
     },
   },
 }
