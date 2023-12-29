c0b92dba241b6d66e0278e1e4547120b3a885893
Flo Schuessel <mail@florian-schuessel.de>
2023-12-29 17:25:57 +0100

    Add codeium

--- config/FoShizzle/lua/plugins/ai.lua
+++ config/FoShizzle/lua/plugins/ai.lua
@@ -1,9 +1,21 @@
 return {
-  "David-Kunz/gen.nvim",
-  opts = {
-    model = "zephyr",
+  {
+    "Exafunction/codeium.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      require("codeium").setup()
+    end,
   },
-  keys = {
-    { "<leader>A", "<cmd>Gen<cr>", mode = { "n", "v" }, desc = "Open AI Prompt" },
+  {
+    "David-Kunz/gen.nvim",
+    opts = {
+      model = "zephyr",
+    },
+    keys = {
+      { "<leader>A", "<cmd>Gen<cr>", mode = { "n", "v" }, desc = "Open AI Prompt" },
+    },
   },
 }
