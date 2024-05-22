ccc43ca2afd3f3a8ba2aa9c2d34f8e2c35c61c29
Flo Schuessel <mail@florian-schuessel.de>
2024-05-22 16:24:24 +0200

    Change indent line

--- config/FoShizzle/lua/plugins/ui.lua
+++ config/FoShizzle/lua/plugins/ui.lua
@@ -177,18 +177,18 @@ return {
   },
 
   -- indent guides for Neovim
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    version = "2.20.8",
-    opts = {
-      -- char = "▏",
-      char = "│",
-      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
-    },
-  },
+  -- {
+  --   "lukas-reineke/indent-blankline.nvim",
+  --   event = { "BufReadPost", "BufNewFile" },
+  --   version = "2.20.8",
+  --   opts = {
+  --     -- char = "▏",
+  --     char = "│",
+  --     filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+  --     show_trailing_blankline_indent = false,
+  --     show_current_context = false,
+  --   },
+  -- },
 
   -- active indent guide and indent text objects
   {
@@ -196,6 +196,11 @@ return {
     version = false, -- wait till new 0.7.0 release to put it back on semver
     event = { "BufReadPre", "BufNewFile" },
     opts = {
+      draw = {
+        animation = function()
+          return 0
+        end,
+      },
       -- symbol = "▏",
       symbol = "│",
       options = { try_as_border = true },
