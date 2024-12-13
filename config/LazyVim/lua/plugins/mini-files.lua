489f45739a5cfe927e4f63f6b592e1905e670508
Flo Schuessel <mail@florian-schuessel.de>
2024-12-13 16:30:09 +0100

    Add config mini files

---
+++ config/LazyVim/lua/plugins/mini-files.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "echasnovski/mini.files",
+    opts = {
+      windows = {
+        width_focus = 40,
+        width_preview = 60,
+      },
+      options = {
+        -- Whether to use for editing directories
+        -- Disabled by default in LazyVim because neo-tree is used for that
+        use_as_default_explorer = false,
+      },
+    },
+  },
+}
