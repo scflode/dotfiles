66ab1f2ec549d11987b7898bf9cdac1d887ddaeb
Flo Schuessel <mail@florian-schuessel.de>
2024-12-13 17:05:44 +0100

    Pin lspconfig to non-broken commit

--- config/LazyVim/lua/plugins/lspconfig.lua
+++ config/LazyVim/lua/plugins/lspconfig.lua
@@ -1,6 +1,7 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    commit = "124be12d782d656b3c75b513a44d9e4728406078", -- Until https://github.com/neovim/nvim-lspconfig/issues/3503 is resolved
     opts = {
       inlay_hints = { enabled = true },
       capabilities = {
