eba19a5feb6c97e9bc2246fdf2d748f686bdcaea
Flo Schuessel <mail@florian-schuessel.de>
2024-12-29 18:53:25 +0100

    Calm down diagnostics

--- config/LazyVim/lua/plugins/lspconfig.lua
+++ config/LazyVim/lua/plugins/lspconfig.lua
@@ -11,6 +11,20 @@ return {
           },
         },
       },
+      diagnostics = {
+        underline = true,
+        update_in_insert = false,
+        virtual_text = false, -- Disable virtual text
+        severity_sort = true,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
+            [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
+            [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
+            [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
+          },
+        },
+      },
     },
   },
 }
