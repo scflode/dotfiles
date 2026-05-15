4700a1693e59995bd9e17649ae490ee6373e2742
Flo Schuessel <mail@florian-schuessel.de>
2026-05-15 09:00:03 +0200

    Add new configs

---
+++ config/LazyVim/lua/plugins/conform.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
+      json = { "prettier" },
+      jsonc = { "prettier" },
+    },
+  },
+}
--- config/LazyVim/lua/plugins/git.lua
+++ config/LazyVim/lua/plugins/git.lua
@@ -11,6 +11,19 @@ return {
       process_spinner = false,
     },
   },
+  {
+    "otavioschwanck/github-pr-reviewer.nvim",
+    opts = {
+      next_hunk_key = "<C-d>",
+      prev_hunk_key = "<C-u>",
+      next_file_key = "<C-n>",
+      prev_file_key = "<C-p>",
+    },
+    keys = {
+      { "<leader>p", "<cmd>PRReviewMenu<cr>", desc = "PR Review Menu" },
+      { "<leader>p", ":<C-u>'<,'>PRSuggestChange<CR>", desc = "Suggest change", mode = "v" },
+    },
+  },
   -- {
   --   "ldelossa/gh.nvim",
   --   dependencies = {
